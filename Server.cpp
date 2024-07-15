a6df3ec5e36792f5136723b209ce87d378bfeef8
Dean S Horak <dean.horak@gmail.com>
2024-07-15 13:26:23 -0500

    Add Copyright Headers

--- Server.cpp
+++ Server.cpp
@@ -1,3 +1,24 @@
+/*
+ * Proprietary License
+ * 
+ * Copyright (c) 2024 Dean S Horak
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of Dean S Horak ("Proprietary Information").
+ * You shall not disclose such Proprietary Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with Dean S Horak.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are not permitted
+ * without express written permission from Your Name.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * If you have any questions about this license, please contact Your Name at dean.horak@gmail.com.
+ */
+
 #include <boost/thread.hpp>
 #include <iostream>
 #include <string>
